36ec129556f95c88101e2237e46ab1a853ecf3d4
Egor Gulid <egor-gulid@yandex.ru>
2022-06-06 20:56:38 +0300

    fix: modalHendler

--- js/main.js
+++ js/main.js
@@ -1,25 +1,21 @@
-import { modalHandler } from "./modules/modalHandler"
+import { modalHandler, modalHandlers } from "./modules/modalHandler"
 import { menu } from "./modules/menu"
 import { forms } from "./modules/forms"
 // import { sendForm } from "./modules/sendForm"
 import { slider } from "./modules/slider"
 import { scrollUp } from "./modules/scrollUp"
 
-export const main = () => {
-    // modalHandler('.mobile-menu .callback-btn', 'block')
-    modalHandler('.header .callback-btn', 'block', {
-        modalSelector: '.modal-callback',
-        modalOverlaySelector: '.modal-overlay'
-    })
-    modalHandler('.modal-close', 'none', {
-        modalSelector: '.modal-callback',
-        modalOverlaySelector: '.modal-overlay'
-    })
-    modalHandler('.quest-section .button-services', 'block', {
-        modalSelector: '.modal-callback',
-        modalOverlaySelector: '.modal-overlay'
-    })
 
+export const main = () => {
+    const defaultModalMeta = {
+        modalSelector: '.modal-callback',
+        modalOverlaySelector: '.modal-overlay'
+    }
+    // modalHandler('.mobile-menu .callback-btn', 'block')
+    modalHandler('.header .callback-btn', 'block', defaultModalMeta)
+    modalHandler('.modal-close', 'none', defaultModalMeta)
+    modalHandler('.quest-section .button-services', 'block', defaultModalMeta)
+    modalHandlers('.services-carousel .element', 'block', defaultModalMeta)
     menu()
     forms()
     // sendForm()
--- js/modules/modalHandler.js
+++ js/modules/modalHandler.js
@@ -10,6 +10,19 @@ export const modalHandler = (selector, display, modalMeta) => {
         modalOverlay.style.display = display
 
     })
-
 }
 
+export const modalHandlers = (selector, display, modalMeta) => {
+    const buttons = document.querySelectorAll(selector)
+    buttons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            e.preventDefault()
+            const modal = document.querySelector(modalMeta.modalSelector)
+            const modalOverlay = document.querySelector(modalMeta.modalOverlaySelector)
+
+            modal.style.display = display
+            modalOverlay.style.display = display
+
+        })
+    })
+}
